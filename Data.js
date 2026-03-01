48e47087388fb114d88554ac01220492c5df19de
GodugunuruSameera <samerra.0909@gmail.com>
2026-03-01 13:39:25 +0530

    Create Data.js

---
+++ Data.js
@@ -0,0 +1,8 @@
+const libraryBooks = [
+  { id: 101, title: "The Alchemist", author: "Paulo Coelho", category: "Fiction", img: "https://m.media-amazon.com/images/I/61HAE8zahLL._AC_UF1000,1000_QL80_.jpg" },
+  { id: 102, title: "A Brief History of Time", author: "Stephen Hawking", category: "Science", img: "https://m.media-amazon.com/images/I/81pDJEOGl1L._AC_UF1000,1000_QL80_.jpg" },
+  { id: 103, title: "Sapiens", author: "Yuval Noah Harari", category: "History", img: "https://m.media-amazon.com/images/I/713jIoMO3UL._AC_UF1000,1000_QL80_.jpg" },
+  { id: 104, title: "Atomic Habits", author: "James Clear", category: "Self Help", img: "https://m.media-amazon.com/images/I/81YkqyaFVEL._AC_UF1000,1000_QL80_.jpg" },
+  { id: 105, title: "1984", author: "George Orwell", category: "Fiction", img: "https://m.media-amazon.com/images/I/71kxa1-0mfL._AC_UF1000,1000_QL80_.jpg" },
+  { id: 106, title: "Cosmos", author: "Carl Sagan", category: "Science", img: "https://m.media-amazon.com/images/I/81577a1iY4L._AC_UF1000,1000_QL80_.jpg" }
+];
